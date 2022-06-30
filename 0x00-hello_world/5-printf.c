c254e992c0a51e0f84e46e3794c5286b2355fdd0
yosephtesfaye <tewdemam@gmail.com>
2022-06-30 13:13:28 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-#include <stdio.h>                                                   /*                                                                 
-								      *
- *  * return 0                                                                                                       
+#include <stdio.h>
+/**
+ * printf
  *
- *   */                                                                                                                                         
-
-int main(void)                                                                                                                                         
-
-{                                                                                                                                              printf("with proper grammer, but the outcome is a peof art,\n");                                                                                                                                                    return (0);
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
