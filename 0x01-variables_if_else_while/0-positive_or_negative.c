38bba88388815f5adb0b1e699a7b6abb57c09873
yosephtesfaye <tewdemam@gmail.com>
2022-07-01 12:00:24 -0700

    control

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,16 +15,16 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-	printf("is positive");
+	printf("%i is positive\n", n);
 }
 else
 if (n == 0)
 {
-	printf("is zero");
+	printf("%i is zero\n", n);
 }
 else
 {
-	printf("is negative");
+	printf("%i is negative\n", n);
 }
 return (0);
 
