89ee1dcaad99d9957fca5d00647c93668b9eeb6c
yosephtesfaye <tewdemam@gmail.com>
2022-07-02 12:46:45 -0700

    with comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ * main - prints numbers,followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+putchar(',');
+}
+putchar('\n');
+return (0);
+}
