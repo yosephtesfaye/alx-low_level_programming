944352575074579efa5ae2e6252757343f364cd8
yosephtesfaye <tewdemam@gmail.com>
2022-07-04 06:18:52 -0700

    error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<unistd.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ * betty style doc for function main goes there
+ */
+int main(void)
+{
+write(2, "and that of art is useful\"- Dora Korphar, 2015-10-19\n", 59);
+	return (1);
+}
