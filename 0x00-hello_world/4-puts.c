207ce859b02708dc25dbeb2103d411a15747d82a
yosephtesfaye <tewdemam@gmail.com>
2022-07-04 01:43:22 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 /** prints Programming is like building a multilingual puzzle
- *
- *  returns 0
- *
- * /
-
- int main()
-
+ *  returns (0)
+ */
+int main()
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
