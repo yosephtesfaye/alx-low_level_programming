31a3d93870a09a0cf498d114cf2d26e2f8a4317c
yosephtesfaye <tewdemam@gmail.com>
2022-07-06 13:55:52 -0700

    10times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,12 +7,12 @@
  */
 void print_alphabet_x10(void)
 {
-	char letter = 'a';
+	char letter;
 	int i;
 
 	for (i = 1; i <= 10; i++)
 	{
-		while (letter <= 'z')
+		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
 			letter++;
