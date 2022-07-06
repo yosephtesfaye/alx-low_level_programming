0759b842e2bf767daaf7b4fcdabc1c36f6729fae
yosephtesfaye <tewdemam@gmail.com>
2022-07-06 13:32:29 -0700

    10times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,7 +4,7 @@
  *                                                                                
  * Return: void                                                                   
  */                                                                               
-void print_alphabet(void)                                                        
+void print_alphabet_x10(void)                                                        
 {                                                                                
 	 char letter = 'a';                                                                int i=1;
 
