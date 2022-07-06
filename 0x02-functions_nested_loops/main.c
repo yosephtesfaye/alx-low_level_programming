7c3484e5a0114a90becf5ac3662dd71e24b31f4b
yosephtesfaye <tewdemam@gmail.com>
2022-07-06 12:15:18 -0700

    put

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * main -calls other 
+ * Return:0
+ */
+int main(void)
+{
+	printsomething();
+	return 0;
+}
---
+++ 0x02-functions_nested_loops/printsomething.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * printsomething -prints something
+ *
+ * Return: nothing
+ */
+void printsomething(void)
+{
+	_putchar('s');
+	_putchar('\n');
+	return;
+}
