12abe1f785557985336397e3e649603c84fbb20a
yosephtesfaye <tewdemam@gmail.com>
2022-07-07 06:51:45 -0700

    1to 3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @cc: c is a number
+ * Return: no return
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - prints the numbers, form 0 to 9
+ * Return: no return
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = 48; i <= 58; i++)
+	{
+		_putchar(i);
+	}
+		_putchar('\n');
+}
