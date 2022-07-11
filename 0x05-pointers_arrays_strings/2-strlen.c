3e096cb23cd5214e21bf2ee03513abb704fef779
yosephtesfaye <tewdemam@gmail.com>
2022-07-11 05:07:36 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - count the lengh of a sting
+ * @s: takes a parameter
+ * Return: int value of lenth
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(s);
+	return (len);
+}
