ed16e66ccb99b1d6490446203914841c59b52619
yosephtesfaye <tewdemam@gmail.com>
2022-07-21 11:50:00 -0700

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,17 +6,20 @@
  */
 int is_prime_number(int n)
 {
-	int i;
+	return (is_prime(n, 2));
+}
 
+int is_prime(int n, int i)
+{
 	if (n <= 1)
 		return (0);
-
-	for (i = 2; i < n; i++)
+	else if (n % i == 0)
+		return (0);
+	else
 	{
-		if (n % i == 0)
-			return (0);
-	}
+	is_prime(n, i + 1);
 	return (1);
+	}
 }
 
 
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,4 +9,5 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int calculate(int num, int guess);
 int is_prime_number(int n);
+int is_prime(int n, int i);
 #endif
