f5ad866bf6de9d56581ee27155abe278cc5eb9a1
yosephtesfaye <tewdemam@gmail.com>
2022-07-27 01:59:48 -0700

    duplicate memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include"main.h"
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * *_strdup - return a new referese which get the duplicate
+ * * of a given sting
+ * @str: pointer reference
+ * Retrun: characer refrernece or NULL
+ */
+
+char *_strdup(char *str)
+{
+	int size = sizeof(str) - 1;
+	char *ne;
+	int i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	ne = (char *)malloc(size * sizeof(char));
+	for (i = 0; i < size; i++)
+	{
+		ne[i] = str[i];
+	}
+	return (ne);
+}
