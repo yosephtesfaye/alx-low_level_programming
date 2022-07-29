299b9a6c10807ecc067ef64d06b93cfe96d9a7b2
yosephtesfaye <tewdemam@gmail.com>
2022-07-29 10:11:56 -0700

    table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,18 +6,41 @@
  */
 void print_times_table(int n)
 {
-	int i, j;
+	int i, j, product;
 
 	for(i = 0; i <= n; i++)
 	{
 		for(j = 0; j <= n; j++)
 		{
-			_putchar(j * i +'0');
-			if(j < n)
+			product = i * j;
+
+			if (j != 0)
 			{
 				_putchar(',');
+				_putchar(' ');
 			}
+			if (product < 10 && j != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((product % 10) + '0');
+			}
+			else if (product >= 10 && product < 100)
+			{
+				_putchar(' ');
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+			else if(product >= 100 && j != 0)
+			{
+				_putchar((product / 100) + '0');
+				_putchar((product / 10) % 10 + '0');
+				_putchar((product % 10) + '0');
+			}
+			else
+				_putchar((product % 10) + '0');
 		}
 		_putchar('\n');
+		}
 	}
-}
+
