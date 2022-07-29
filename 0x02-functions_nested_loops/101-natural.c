b4c4697c4610a63e2fd5f3a45fa5d2c9e08c6e5d
yosephtesfaye <tewdemam@gmail.com>
2022-07-29 11:18:45 -0700

    multi

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,13 +6,14 @@
 int main(void)
 {
 	int i;
-       int sum = 0;
-	
-	for(i = 3; i <= 1024; i++)
+
+	int sum = 0;
+
+	for (i = 3; i <= 1024; i++)
 	{
-		if(i % 3 == 0 || i % 5 == 0)
+		if (i % 3 == 0 || i % 5 == 0)
 			sum += i;
 	}
-	printf("%d\n",sum);
+	printf("%d\n", sum);
 	return (0);
 }
