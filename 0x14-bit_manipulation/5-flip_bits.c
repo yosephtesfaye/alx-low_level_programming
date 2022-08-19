aaca195d47cb4dad6b790747048558fd040965ab
yosephtesfaye <tewdemam@gmail.com>
2022-08-19 13:04:43 -0700

    fleep

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * flip_bits - flip number
+ * @n: unsigned long int
+ * @m: unsigned long int
+ * Return: unsigned int
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int mask = 0, index;
+
+	index = n ^ m;
+
+	while (index > 0)
+	{
+		if (index & 1)
+		{
+			mask++;
+		}
+		index >>= 1;
+	}
+	return (mask);
+}
